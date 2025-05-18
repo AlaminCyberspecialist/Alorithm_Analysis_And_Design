78288146c070ee226831d637a2b7392cb7450379
MD.AL-AMIN <mdalamin7e4@gmail.com>
2025-05-18 15:25:45 +0600

    Huffman coding

--- App.java
+++ App.java
@@ -1,62 +1,62 @@
+
 import java.util.*;
-import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-       
-        ArrayList<ActivitySelection>list = new ArrayList<>();
 
-        ActivitySelection act1 = new ActivitySelection();
-        act1.setActivity("A1");
-        act1.setStart(0);
-        act1.setFinish(6);
-
-        ActivitySelection act2 = new ActivitySelection();
-        act2.setActivity("A2");
-        act2.setStart(3);
-        act2.setFinish(4);
-
-        ActivitySelection act3 = new ActivitySelection();
-        act3.setActivity("A3");
-        act3.setStart(1);
-        act3.setFinish(2);
-
-        ActivitySelection act4 = new ActivitySelection();
-        act4.setActivity("A4");
-        act4.setStart(5);
-        act4.setFinish(9);
-
-        ActivitySelection act5 = new ActivitySelection();
-        act5.setActivity("A5");
-        act5.setStart(5);
-        act5.setFinish(7);
-
-        ActivitySelection act6 = new ActivitySelection();
-        act6.setActivity("A6"); 
-        act6.setStart(8);
-        act6.setFinish(9);
-
-        list.add(act1);
-        list.add(act2);
-        list.add(act3);
-        list.add(act4);
-        list.add(act5);
-        list.add(act6);
-
-
-        Collections.sort(list,new ASPcomparator());
-        System.out.println("Selected Activities: ");
-
-
-        int i = list.get(0).getFinish();
-        System.out.print(list.get(0).getActivity()+" ");
-
-        for(ActivitySelection a:list){
-            if(i<a.getStart()){
-                System.out.print(a.getActivity()+ " ");
-                i = a.getStart();
-            }
+    public static void printCode(HuffmanNode root,String s)
+    {
+        if(root.left==null && root.right==null && Character.isLetter(root.c) ){
+            System.out.println(root.c+" "+s);
+            return;
         }
-
+        printCode(root.left, s+"0");
+        printCode(root.right,s+"1");
     }
+    public static void main(String[] args) {
+       
+        int n =6;
+        char[] charArray = {'a','b','c','d','e','f'};
+
+        int[] charfreq={5,9,12,13,16,45};
+
+        PriorityQueue<HuffmanNode>q = new PriorityQueue<HuffmanNode>(n,new HuffComparator());
+
+        for(int i=0;i<n;i++)
+        {
+            HuffmanNode hn = new HuffmanNode();
+            hn.c = charArray[i];
+            hn.data = charfreq[i];
+
+            hn.left =null;
+            hn.right=null;
+
+            q.add(hn);
+        }
+       
+        HuffmanNode root =new HuffmanNode();
+
+        while(q.size() > 1){
+            HuffmanNode x = q.peek();
+            q.poll();
+            HuffmanNode y = q.peek();
+            q.poll();
+
+
+        
+        HuffmanNode f =new HuffmanNode();
+
+        f.data =x.data + y.data;
+
+        f.c = '-';
+        f.left=x;
+        f.right =y;
+        root=f;
+        q.add(f);
+
+
+        }printCode(root," ");
+    }
+
+   
 }
---
+++ HuffComparator.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+public class HuffComparator implements Comparator<HuffmanNode> {
+
+    @Override
+    public int compare(HuffmanNode  x, HuffmanNode y) {
+        // TODO Auto-generated method stub
+        return  x.data - y.data;
+    }
+
+ 
+    
+    
+    
+
+}
