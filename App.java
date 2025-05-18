7a8a7deaa2306a7c4546802b9b3cd995521a594a
MD.AL-AMIN <mdalamin7e4@gmail.com>
2025-05-18 15:27:20 +0600

    BFS

--- App.java
+++ App.java
@@ -1,18 +1,20 @@
-public class App {
-    public static void main(String[] args) {
-        int graph[][] = {
-            { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-            { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-            { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-            { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-            { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-            { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-            { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-            { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-            { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
-        };
 
-        ShortestPath sp = new ShortestPath();
-        sp.dijkstra(graph, 0); // Start from vertex 0
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+Graph g =new Graph(7);
+
+g.addEdge(0,1);
+
+g.addEdge(0,2);
+g.addEdge(1,3);
+g.addEdge(1,4);
+g.addEdge(2,5);
+g.addEdge(2,6);
+
+System.out.println("BFS starting from vertice 0: ");
+
+g.BFS(0);
     }
 }
