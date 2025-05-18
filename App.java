07c9365d5d7b07e8f4177bf5a656ef28867e0b9b
MD.AL-AMIN <mdalamin7e4@gmail.com>
2025-05-18 15:27:55 +0600

    DFS

--- App.java
+++ App.java
@@ -1,20 +1,19 @@
-
 import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
        
-Graph g =new Graph(7);
 
-g.addEdge(0,1);
+        Graph g = new Graph(7);
 
-g.addEdge(0,2);
-g.addEdge(1,3);
-g.addEdge(1,4);
-g.addEdge(2,5);
-g.addEdge(2,6);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(2,5);
+        g.addEdge(2,6);
+       
+        System.out.println("DFS starting from vertex 0: ");
 
-System.out.println("BFS starting from vertice 0: ");
-
-g.BFS(0);
+        g.DFS(0);
     }
 }
--- Graph.java
+++ Graph.java
@@ -1,45 +1,36 @@
 import java.util.*;
 
 public class Graph {
-    private int vertices;
-    private LinkedList<Integer>[]adjacencyList;
 
-    public Graph(int vertices){
-        this.vertices =vertices;
-        this.adjacencyList=new  LinkedList[vertices];
+private int vertices;
+private LinkedList<Integer>[]adjacencyList;
 
-        for(int i=0;i<vertices;i++){
-            this.adjacencyList[i] =new LinkedList<>();
+public Graph(int vertices){
+    this.vertices = vertices;
+    this.adjacencyList = new LinkedList[vertices];
+
+    for(int i=0;i<vertices;i++){
+        this.adjacencyList[i] =new LinkedList<>();
+    }
+}
+public void addEdge(int from,int to){
+    adjacencyList[from].add(to);
+}
+
+public void DFS(int start){
+    boolean[] visited = new boolean[vertices];
+
+    DFSUtil(start,visited);
+}
+public void DFSUtil(int curr,boolean[] visited){
+    visited[curr] = true;
+    System.out.println(curr+ " ");
+
+    for(int neigh:adjacencyList[curr]){
+        if(!visited[neigh]){
+            DFSUtil(neigh, visited);
         }
     }
-
-    public void addEdge(int from,int to){
-        adjacencyList[from].add(to);
-    }
-
-
-
-
-
-    public void  BFS(int start){
-
-        boolean[]  visited = new boolean[vertices];
-        Queue<Integer>q=new LinkedList<>();
-
-        visited[start] =true;
-
-        q.add(start);
-
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            System.out.println(curr+ " ");
-
-            for(int neigh:adjacencyList[curr]){
-                if(!visited[neigh]){
-                    visited[neigh] = true;
-                    q.add(neigh);
-                }
-            }
-        }
-    }
+}
+    
 }
